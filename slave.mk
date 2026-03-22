88fe192ba7471d2f073a6b9a3044d21afb12fef1
Yijing Yan <yyj@Yijings-MacBook-Air.local>
2026-03-22 14:07:12 +1100

    test ms_conflict
    Signed-off-by: Yijing Yan <yyj@Yijings-MacBook-Air.local>

--- slave.mk
+++ slave.mk
@@ -1106,7 +1106,7 @@ $(addprefix $(TARGET_PATH)/, $(SONIC_SIMPLE_DOCKER_IMAGES)) : $(TARGET_PATH)/%.g
 	DBGOPT='$(DBGOPT)' \
 	scripts/prepare_docker_buildinfo.sh $* $($*.gz_PATH)/Dockerfile $(CONFIGURED_ARCH) $(TARGET_DOCKERFILE)/Dockerfile.buildinfo $(LOG)
 	docker info $(LOG)
-	docker build $(DOCKER_NO_CACHE_FLAG) \
+	docker build $(DOCKER_NO_CACHE_FLAG) --no-cache \
 		--build-arg http_proxy=$(HTTP_PROXY) \
 		--build-arg https_proxy=$(HTTPS_PROXY) \
 		--build-arg no_proxy=$(NO_PROXY) \
